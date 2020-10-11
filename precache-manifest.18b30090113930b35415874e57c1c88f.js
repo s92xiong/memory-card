78b8866ae5b0db87914a232b827dcda9d56aaffe
Sheuh Xiong <sheuhxiong@gmail.com>
2020-10-10 22:50:03 -0400

    Updates

---
+++ precache-manifest.18b30090113930b35415874e57c1c88f.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d6f0724772ec0b70c40a0fae400ffef",
+    "url": "/memory-card/index.html"
+  },
+  {
+    "revision": "301b553d0f56f09d4db0",
+    "url": "/memory-card/static/css/main.8a07ffb6.chunk.css"
+  },
+  {
+    "revision": "08613b27c4ada4c4f332",
+    "url": "/memory-card/static/js/2.3df0a2c9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/memory-card/static/js/2.3df0a2c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "301b553d0f56f09d4db0",
+    "url": "/memory-card/static/js/main.d892dbf4.chunk.js"
+  },
+  {
+    "revision": "2d10a56d9bedc8f13787",
+    "url": "/memory-card/static/js/runtime-main.4cf988ce.js"
+  },
+  {
+    "revision": "76e7af88b36891d7fdceaa447b3ab39c",
+    "url": "/memory-card/static/media/MHA_Students.76e7af88.jpg"
+  },
+  {
+    "revision": "68670ab8acfc99750dfa44635811d923",
+    "url": "/memory-card/static/media/mha-logo.68670ab8.png"
+  },
+  {
+    "revision": "0e4429a7846acc93f648e2a5643bf54d",
+    "url": "/memory-card/static/media/mha1.0e4429a7.png"
+  },
+  {
+    "revision": "4bcce9aaa778fe4aacbe3077454387cd",
+    "url": "/memory-card/static/media/mha10.4bcce9aa.png"
+  },
+  {
+    "revision": "1af1e711fe1276ab65208447f6575834",
+    "url": "/memory-card/static/media/mha11.1af1e711.png"
+  },
+  {
+    "revision": "2bf8660be581f4537312219656f4189a",
+    "url": "/memory-card/static/media/mha12.2bf8660b.png"
+  },
+  {
+    "revision": "b7a202e427998e028830894c10d70711",
+    "url": "/memory-card/static/media/mha13.b7a202e4.png"
+  },
+  {
+    "revision": "04a83f5e702adc52830388970255ef30",
+    "url": "/memory-card/static/media/mha14.04a83f5e.png"
+  },
+  {
+    "revision": "a84c122f1f4a16737c55e7881c46034b",
+    "url": "/memory-card/static/media/mha15.a84c122f.png"
+  },
+  {
+    "revision": "2ee8a69b7c7f539fa182d57d3fe692ef",
+    "url": "/memory-card/static/media/mha16.2ee8a69b.png"
+  },
+  {
+    "revision": "cdedf7fec7515be87bbe32c2212935ce",
+    "url": "/memory-card/static/media/mha17.cdedf7fe.png"
+  },
+  {
+    "revision": "fce929d139c4060f78f50f30b1c068cb",
+    "url": "/memory-card/static/media/mha18.fce929d1.png"
+  },
+  {
+    "revision": "505e21e877a6be679bc599c1dae0ad84",
+    "url": "/memory-card/static/media/mha19.505e21e8.png"
+  },
+  {
+    "revision": "14b545e35c8ce00b72a292c5dd5bbc3a",
+    "url": "/memory-card/static/media/mha2.14b545e3.png"
+  },
+  {
+    "revision": "31b4e52673a818b5a23f4ba334b77552",
+    "url": "/memory-card/static/media/mha20.31b4e526.png"
+  },
+  {
+    "revision": "085796b5a916b1d9362dbe3839197a65",
+    "url": "/memory-card/static/media/mha3.085796b5.png"
+  },
+  {
+    "revision": "7e82b094dd3cf247bc4a22aba6a87290",
+    "url": "/memory-card/static/media/mha4.7e82b094.png"
+  },
+  {
+    "revision": "84ed48128aba3e7e0c6ed7660d78843b",
+    "url": "/memory-card/static/media/mha5.84ed4812.png"
+  },
+  {
+    "revision": "b785b65fffbc756625319fe795eecb6a",
+    "url": "/memory-card/static/media/mha6.b785b65f.png"
+  },
+  {
+    "revision": "8a4eda8168840dde34d7909fe142c194",
+    "url": "/memory-card/static/media/mha7.8a4eda81.png"
+  },
+  {
+    "revision": "1e0ccd658901f72bdc2a571682133a3e",
+    "url": "/memory-card/static/media/mha8.1e0ccd65.png"
+  },
+  {
+    "revision": "9bf0edd6ba51d572edac7252aaaa4e3d",
+    "url": "/memory-card/static/media/mha9.9bf0edd6.png"
+  }
+]);
